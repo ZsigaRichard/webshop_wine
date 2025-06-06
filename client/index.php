dea159ccc9190511e93895bd3dc5797e75eb58e7
ZsigaRichard <zsigarichard0406@gmail.com>
2025-06-07 01:25:31 +0200

    dekstop menu

--- client/index.php
+++ client/index.php
@@ -5,8 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 </head>
-<body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<body  class="fs-5">
+<nav class="navbar bg-light sticky-top">
+  <div class="container-fluid d-flex justify-content-between align-items-center px-3">
+
+    <a class="navbar-brand" href="#">Logo</a>
+
+    <ul class="nav flex-wrap">
+      <li class="nav-item"><a class="nav-link px-2" href="#">Programok</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Rozé borok</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Mustok</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Gyöngyöző bor</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Fehérborok</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Kapcsolat</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Segítség</a></li>
+      <li class="nav-item"><a class="nav-link px-2" href="#">Rólunk</a></li>
+    </ul>
+
+    <div class="d-flex gap-3">
+      <a href="#" class="nav-link p-0"><i class="fas fa-user"></i></a>
+      <a href="#" class="nav-link p-0"><i class="fas fa-search"></i></a>
+      <a href="#" class="nav-link p-0"><i class="fas fa-shopping-cart"></i></a>
+    </div>
+
+  </div>
+</nav>
+
 </body>
+
 </html>

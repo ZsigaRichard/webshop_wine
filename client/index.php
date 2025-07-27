c1416c498b2dcd5fc7b3cc96ec2d1ab2db9bdf53
ZsigaRichard <zsigarichard0406@gmail.com>
2025-07-27 20:30:38 +0200

    menu

--- client/index.php
+++ client/index.php
@@ -3,36 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <title>Bor webshop</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" />
 
 </head>
-<body  class="fs-5">
-<nav class="navbar bg-light sticky-top">
-  <div class="container-fluid d-flex justify-content-between align-items-center px-3">
+<body>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <!--<img id="logo" src="images/altinger_logo.png" alt="" width="30" height="24">-->
+    <a class="navbar-brand" href="#">Altinger</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-    <a class="navbar-brand" href="#">Logo</a>
-
-    <ul class="nav flex-wrap">
-      <li class="nav-item"><a class="nav-link px-2" href="#">Programok</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Rozé borok</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Mustok</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Gyöngyöző bor</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Fehérborok</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Kapcsolat</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Segítség</a></li>
-      <li class="nav-item"><a class="nav-link px-2" href="#">Rólunk</a></li>
-    </ul>
-
-    <div class="d-flex gap-3">
-      <a href="#" class="nav-link p-0"><i class="fas fa-user"></i></a>
-      <a href="#" class="nav-link p-0"><i class="fas fa-search"></i></a>
-      <a href="#" class="nav-link p-0"><i class="fas fa-shopping-cart"></i></a>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0"> <!-- KÖZÉPRE IGAZÍTÁS -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Főoldal</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="feherDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Fehérborok
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="feherDropdown">
+            <li><a class="dropdown-item" href="#">Fehérbor</a></li>
+            <li><a class="dropdown-item" href="#">Fehérbor</a></li>
+            <li><a class="dropdown-item" href="#">Fehérbor</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="vorosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Vörösborok
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="vorosDropdown">
+            <li><a class="dropdown-item" href="#">Vörösbor</a></li>
+            <li><a class="dropdown-item" href="#">Vörösbor</a></li>
+            <li><a class="dropdown-item" href="#">Vörösbor</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Must</a>
+        </li>
+      </ul>
     </div>
-
   </div>
 </nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
